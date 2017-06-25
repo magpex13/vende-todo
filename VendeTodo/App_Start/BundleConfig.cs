7591326fd9f050e3e9788c6a6c842b9eb6d372a0
Magno Bustamante <magpex13@github.com>
2017-06-24 21:03:14 -0500

    Vista Login

--- VendeTodo/App_Start/BundleConfig.cs
+++ VendeTodo/App_Start/BundleConfig.cs
@@ -9,23 +9,25 @@ namespace VendeTodo
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
-            // preparado para la producción y podrá utilizar la herramienta de compilación disponible en http://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
+                        "~/Content/js/jquery.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Content/js/bootstrap.min.js"));
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                     "~/Content/css/bootstrap.min.css"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+
+            // Login
+            bundles.Add(new ScriptBundle("~/login/scripts").Include(
+                      "~/Content/js/sidebar-nav.min.js",
+                      "~/Content/js/jquery.slimscroll.js",
+                      "~/Content/js/waves.js",
+                      "~/Content/js/custom.min.js",
+                      "~/Content/js/jQuery.style.switcher.js"));
+            bundles.Add(new StyleBundle("~/login/css").Include(
+                      "~/Content/css/animate.css",
+                      "~/Content/css/style.css",
+                      "~/Content/css/blue.css",
+                      "~/Content/css/spinners.css"));
         }
     }
 }
